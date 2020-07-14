23e49227c299ca4ce843ff2e7931d8e2d78eb5b2
Robin Shahan <robinsh@microsoft.com>
2020-07-14 16:51:06 -0700

    Update to .NETCore 3.1

--- horizontal-arm/arm-read-write/Program.cs
+++ horizontal-arm/arm-read-write/Program.cs
@@ -1,33 +1,36 @@
 ﻿// Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-//This code that messages to an IoT Hub for testing the the arm-read-write app is
-//  in Azure-IoT-Samples-CSharp/horizontal-arm/arm-read-write//
-//The ARM template is in the .\arm-template folder.
+//This code that messages to an IoT Hub for testing the routing as defined
+//  in this article: https://docs.microsoft.com/en-us/azure/iot-hub/tutorial-routing
+//The scripts for creating the resources are included in the resources folder in this
+//  Visual Studio solution. 
 
 using Microsoft.Azure.Devices.Client;
 using Newtonsoft.Json;
 using System;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace arm_read_write
 {
-    public class Program
+    class Program
     {
         //
-        //    This is how this tutorial will work. 
-        //
         //    1. Use the ARM template to load the resources for this quickstart.
-        //    2. Create a device on the IoT Hub. Retrieve the device id keys and save them.
+        //    2. Using CLI, create a device on the IoT Hub. Retrieve the keys and save them.
         //    3. Set the routing query on the device to be "true" so all messages will be passed through 
-        //        and written to Azure Storage.
-        //    4. run this application (arm-read-write). It will send messages to the hub, 
+        //        and written to Azure Storage.:
+        //    4. run the simulated device application (arm-read-write). It will submit messages to the hub, 
         //        which will write them all to storage.
         //    5. open the storage account and look at the resulting messages
         //    6. Stop running the console app
         //    7. When finished, delete the resource group
         //
+        // ARM template is called arm-read-write-sample.
+        // put this in the repository so you can just call it to load it 
+        // keep the suffixes so you don't have to worry about duplicates
 
         //This is the code that sends messages to the IoT Hub for routing messages to storage. 
         //  In this case, all the messages will be routed directly to storage and written.
@@ -35,47 +38,36 @@ namespace arm_read_write
         //  https://docs.microsoft.com/en-us/azure/iot-hub/tutorial-routing
 
         private static DeviceClient s_deviceClient;
-
-        //These are environment variables, retrieve them and store them here.
+        private readonly static string s_myDeviceId = "Contoso-Test-Device";
+        private readonly static string s_iotHubUri = "ContosoTestHubdlxlud5h.azure-devices.net";
         // This is the primary key for the device. This is in the portal. 
-        // Find your IoT hub in the portal > IoT devices > select your device > copy the key. 
-        private static string _envIOT_DEVICE_ID = Environment.GetEnvironmentVariable("IOT_DEVICE_ID");
-        private static string _envIOT_HUB_URI = Environment.GetEnvironmentVariable("IOT_HUB_URI");
-        private static string _envIOT_DEVICE_KEY = Environment.GetEnvironmentVariable("IOT_DEVICE_KEY");
+        // Find your IoT hub in the portal > IoT devic1es > select your device > copy the key. 
+        private readonly static string s_deviceKey = "RClD0LGxZCYavagk8tS2M7L1MI5bcKcyR+tJHzj+gDk=";
 
-        public static async Task Main()
+        private static async Task Main()
         {
-            //for testing, just hardcode these
-            _envIOT_HUB_URI = "HUB-NAME-GOES-HERE.azure-devices-net";
-            _envIOT_DEVICE_KEY = "DEVICE-KEY-GOES-HERE";
-            _envIOT_DEVICE_ID = "Contoso-Test-Device";
-            Console.WriteLine("iot hub uri = <{0}>", _envIOT_HUB_URI);
-            Console.WriteLine("iot device id = <{0}>", _envIOT_DEVICE_ID);
-            Console.WriteLine("iot device key = <{0}>", _envIOT_DEVICE_KEY);
+            Console.WriteLine("write messages to a hub and use routing to write them to storage");
+            s_deviceClient = DeviceClient.Create(s_iotHubUri, new DeviceAuthenticationWithRegistrySymmetricKey(s_myDeviceId, s_deviceKey), TransportType.Mqtt);
 
-            Console.WriteLine("starting");
-            s_deviceClient = DeviceClient.Create(_envIOT_HUB_URI, 
-                new DeviceAuthenticationWithRegistrySymmetricKey(_envIOT_DEVICE_ID, _envIOT_DEVICE_KEY), 
-                TransportType.Mqtt);
-            await SendDeviceToCloudMessagesAsync();
+            var cts = new CancellationTokenSource();
 
+            var messages = SendDeviceToCloudMessagesAsync(cts.Token);
             Console.WriteLine("Press the Enter key to stop.");
-
             Console.ReadLine();
-
-
+            cts.Cancel();
+            await messages;
         }
 
         /// <summary>
         /// Send message to the Iot hub. This generates the object to be sent to the hub in the message.
         /// </summary>
-        private static async Task SendDeviceToCloudMessagesAsync()
+        private static async Task SendDeviceToCloudMessagesAsync(CancellationToken token)
         {
             double minTemperature = 20;
             double minHumidity = 60;
             Random rand = new Random();
 
-            while (true)
+            while (!token.IsCancellationRequested)
             {
 
                 double currentTemperature = minTemperature + rand.NextDouble() * 15;
@@ -105,7 +97,7 @@ namespace arm_read_write
 
                 var telemetryDataPoint = new
                 {
-                    deviceId = _envIOT_DEVICE_ID,
+                    deviceId = s_myDeviceId,
                     temperature = currentTemperature,
                     humidity = currentHumidity,
                     pointInfo = infoString
