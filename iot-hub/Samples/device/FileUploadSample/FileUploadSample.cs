38ba20518c7a3d95cba6799467eb553ef421e0dc
Christian Beier <chbeier@microsoft.com>
2020-07-23 21:11:51 +0200

    updated fileupload sample and eplaced obsolete UploadToBlobAsync with GetFileUploadSasUri, Azrue Storage SDK upload, CompleteFileUploadAsync

--- iot-hub/Samples/device/FileUploadSample/FileUploadSample.cs
+++ iot-hub/Samples/device/FileUploadSample/FileUploadSample.cs
@@ -1,10 +1,8 @@
-﻿// Copyright (c) Microsoft. All rights reserved.
-// Licensed under the MIT license. See LICENSE file in the project root for full license information.
-
 using System;
 using System.Diagnostics;
 using System.IO;
 using System.Threading.Tasks;
+using Azure.Storage.Blobs;
 
 namespace Microsoft.Azure.Devices.Client.Samples
 {
@@ -29,11 +27,34 @@ namespace Microsoft.Azure.Devices.Client.Samples
 
                 var watch = Stopwatch.StartNew();
 
-                // Note: UploadToBlobAsync will use HTTPS as protocol, regardless of the DeviceClient protocol selection.
-                await _deviceClient.UploadToBlobAsync(fileName, fileStreamSource).ConfigureAwait(false);
-                watch.Stop();
+                var sasUriReq = new Transport.FileUploadSasUriRequest() { BlobName = fileName };
+                var sasUriRes = await _deviceClient.GetFileUploadSasUriAsync(sasUriReq);
+                var isSuccess = false;
 
-                Console.WriteLine("Time to upload file: {0}ms\n", watch.ElapsedMilliseconds);
+                try {
+                    // Upload the file using BlobClient via HTTPS protocol, regardless of the DeviceClient protocol selection.
+                    var blobClient = new BlobClient(sasUriRes.GetBlobUri());
+
+                    var uploadRes = await blobClient.UploadAsync(fileStreamSource);
+                    isSuccess = (uploadRes.GetRawResponse().Status == 201);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("{0} Exception caught.", e.Message);
+                }
+                finally
+                {
+                    // Notify IoT Hub of a completed file upload.
+                    var uploadNotification = new Transport.FileUploadCompletionNotification() { CorrelationId = sasUriRes.CorrelationId, IsSuccess = isSuccess };
+                    await _deviceClient.CompleteFileUploadAsync(uploadNotification);
+
+                    watch.Stop();
+                    Console.WriteLine("Time to upload file: {0}ms\n", watch.ElapsedMilliseconds);
+                }
+
+                // Explicitly close FileStream
+                Console.WriteLine("FileStream Close.");
+                fileStreamSource.Close();
             }
         }
     }
