ba3e0f999b16990bdd2ffd008007897ab88bae3e
robinsh <robinsh@microsoft.com>
2020-09-11 16:39:01 -0700

    Set body

--- iot-hub/Tutorials/Routing/SimulatedDevice/Program.cs
+++ iot-hub/Tutorials/Routing/SimulatedDevice/Program.cs
@@ -29,10 +29,10 @@ namespace SimulatedDevice
     {
         private static DeviceClient s_deviceClient;
         private readonly static string s_myDeviceId = "Contoso-Test-Device";
-        private readonly static string s_iotHubUri = "<hub-name-goes-here>.azure-devices.net";
+        private readonly static string s_iotHubUri = "ContosoTestHubdlxlud5h.azure-devices.net";
         // This is the primary key for the device. This is in the portal. 
         // Find your IoT hub in the portal > IoT devices > select your device > copy the key. 
-        private readonly static string s_deviceKey = "<device-key-goes-here>";
+        private readonly static string s_deviceKey = "RClD0LGxZCYavagk8tS2M7L1MI5bcKcyR+tJHzj+gDk=";
 
         // If this is false, it will submit messages to the iot hub. 
         // If this is true, it will read one of the output files and convert it to ASCII.
@@ -79,7 +79,7 @@ namespace SimulatedDevice
             double minHumidity = 60;
             Random rand = new Random();
 
-            while (true)
+            while (!token.IsCancellationRequested)
             {
                 double currentTemperature = minTemperature + rand.NextDouble() * 15;
                 double currentHumidity = minHumidity + rand.NextDouble() * 20;
