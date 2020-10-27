fa887fd83ae7bd99754a84b71574597b1506a23c
Abhipsa Misra <abhipsa.misra@microsoft.com>
2020-10-27 16:02:07 -0700

    refactor(iot-service): Update the service client sample to show reconnection behavior

--- iot-hub/Samples/service/ServiceClientSample/ServiceClientSample.cs
+++ iot-hub/Samples/service/ServiceClientSample/ServiceClientSample.cs
@@ -1,8 +1,10 @@
 ﻿// Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
+using Microsoft.Extensions.Logging;
 using System;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Microsoft.Azure.Devices.Samples
@@ -10,20 +12,64 @@ namespace Microsoft.Azure.Devices.Samples
     public class ServiceClientSample
     {
         private readonly ServiceClient _serviceClient;
+        private readonly string _deviceId;
+        private readonly ILogger _logger;
 
-        public ServiceClientSample(ServiceClient serviceClient)
+        public ServiceClientSample(ServiceClient serviceClient, string deviceId, ILogger logger)
         {
             _serviceClient = serviceClient ?? throw new ArgumentNullException(nameof(serviceClient));
+            _deviceId = deviceId ?? throw new ArgumentNullException(nameof(deviceId));
+            _logger = logger;
         }
 
-        public async Task RunSampleAsync(string deviceId)
+        public async Task RunSampleAsync()
         {
-            var str = "Hello, Cloud!";
-            using var message = new Message(Encoding.ASCII.GetBytes(str));
+            using var cts = new CancellationTokenSource();
+            Console.CancelKeyPress += (sender, eventArgs) =>
+            {
+                eventArgs.Cancel = true;
+                cts.Cancel();
+                _logger.LogInformation("Sample execution cancellation requested; will exit.");
+            };
 
-            Console.Write($"\tSending C2D message to {deviceId} . . . ");
-            await _serviceClient.SendAsync(deviceId, message).ConfigureAwait(false);
-            Console.WriteLine("DONE");
+            try
+            {
+                await SendC2DMessagesAsync(cts.Token);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"Unrecoverable exception caught, user action is required, so exiting...: \n{ex}");
+            }
+
+        }
+
+        private async Task SendC2DMessagesAsync(CancellationToken cancellationToken)
+        {
+            int messageCount = 0;
+            while (!cancellationToken.IsCancellationRequested)
+            {
+                var str = $"Hello, Cloud! - Message {++messageCount }";
+                var message = new Message(Encoding.ASCII.GetBytes(str));
+                _logger.LogInformation($"\tSending C2D message {messageCount} with Id {message.MessageId} to {_deviceId} . . . ");
+
+                while (!cancellationToken.IsCancellationRequested)
+                {
+                    try
+                    {
+                        await _serviceClient.SendAsync(_deviceId, message, TimeSpan.FromSeconds(10));
+                        _logger.LogInformation($"Sent message {messageCount}.");
+                        message.Dispose();
+                        break;
+                    }
+                    catch (Exception e) when (ExceptionHelper.IsNetwork(e))
+                    {
+                        _logger.LogError("Transient Exception occurred, will retry...");
+                        await Task.Delay(5 * 1000);
+                    }
+
+                }
+                await Task.Delay(5 * 1000);
+            }
         }
     }
 }
