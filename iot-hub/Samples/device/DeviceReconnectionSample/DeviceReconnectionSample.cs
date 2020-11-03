ac1befbea6fab00ed493387b465653befded43f4
Abhipsa Misra <abhipsa.misra@microsoft.com>
2020-11-03 14:46:46 -0800

    unused using

--- iot-hub/Samples/device/DeviceReconnectionSample/DeviceReconnectionSample.cs
+++ iot-hub/Samples/device/DeviceReconnectionSample/DeviceReconnectionSample.cs
@@ -6,7 +6,6 @@ using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Reflection.Metadata.Ecma335;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
