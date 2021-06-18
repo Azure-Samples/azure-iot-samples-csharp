ba85609d193f8f1311d407a1d80cd847a92c68fd
Abhipsa Misra <abhipsa.misra@microsoft.com>
2021-06-18 10:51:30 -0700

    feat(all): Update samples to use latest relevant version of .NET

--- README.md
+++ README.md
@@ -16,12 +16,14 @@ azure-iot-samples-csharp provides a set of easy-to-understand, continuously-test
 
 These samples are against the `preview` releases of the Azure IoT Hub SDK for .NET. For samples against the GA releases, see [here](https://github.com/Azure-Samples/azure-iot-samples-csharp).
 
+> NOTE: The `preview` branch is meant to hold only those samples that are relevant to the corresponding `preview` feature added in the SDK. Once the `preview` feature is GA'd, these samples will be moved over to the main branch. This branch is not intended to be directly merged into the main branch.
+
 ## Prerequisites
 
-- .NET 5.0 on your development machine.  You can download the .NET Core SDK for multiple platforms from [.NET](https://dotnet.microsoft.com/download/dotnet/5.0).  You can verify the current version of C# on your development machine using 'dotnet --version'.
+- .NET 5.0 on your development machine.  You can download the .NET 5.0 SDK for multiple platforms from [.NET](https://dotnet.microsoft.com/download/dotnet/5.0).  You can verify the current version of .NET on your development machine using 'dotnet --version'.
 
 ## Resources
 
-- [azure-iot-sdk-csharp](https://github.com/Azure/azure-iot-sdk-csharp/tree/preview): contains the `preview` source code for Azure IoT Hub SDK for .NET.
+- [azure-iot-sdk-csharp](https://github.com/Azure/azure-iot-sdk-csharp/tree/preview): contains the `preview` source code for Azure IoT Hub SDK for .NET
 - [Azure IoT Hub Documentation](https://docs.microsoft.com/azure/iot-hub/)
 - [Get-started](https://docs.microsoft.com/azure/iot-hub/quickstart-send-telemetry-dotnet)
--- build.ps1
+++ build.ps1
@@ -52,7 +52,7 @@ Function BuildProject($path, $message) {
 
     Write-Host
     Write-Host -ForegroundColor Cyan $label
-    cd (Join-Path $rootDir $path)
+    Set-Location (Join-Path $rootDir $path)
 
     if ($clean) {
         & dotnet clean --verbosity $verbosity --configuration $configuration
@@ -74,7 +74,7 @@ Function RunApp($path, $message, $params) {
 
     Write-Host
     Write-Host -ForegroundColor Cyan $label
-    cd (Join-Path $rootDir $path)
+    Set-Location (Join-Path $rootDir $path)
 
     $runCommand = "dotnet run -- $params"
     Invoke-Expression $runCommand
@@ -95,8 +95,6 @@ try {
         BuildProject iot-hub\Samples\device "IoTHub Device Samples"
         BuildProject iot-hub\Samples\module "IoTHub Module Samples"
         BuildProject iot-hub\Samples\service "IoTHub Service Samples"
-        BuildProject iot-hub\Quickstarts "IoTHub Device Quickstarts"
-        BuildProject iot-hub\Tutorials\Routing "IoTHub Tutorials - Routing"
         BuildProject provisioning\Samples\device "Provisioning Device Samples"
         BuildProject provisioning\Samples\service "Provisioning Service Samples"
         BuildProject security\Samples "Security Samples"
@@ -110,6 +108,7 @@ try {
         RunApp provisioning\Samples\service\CleanupEnrollmentsSample "Provisioning\Service\CleanupEnrollmentsSample"
         RunApp iot-hub\Samples\service\CleanupDevicesSample "IoTHub\Service\CleanupDevicesSample" "-c ""$env:IOTHUB_CONNECTION_STRING"" -a ""$env:STORAGE_ACCOUNT_CONNECTION_STRING"" --PathToDevicePrefixForDeletion ""$env:PATH_TO_DEVICE_PREFIX_FOR_DELETION_FILE"""
 
+        # Run the iot-hub\device samples
         RunApp iot-hub\Samples\device\DeviceReconnectionSample "IoTHub\Device\DeviceReconnectionSample" "-p ""$env:IOTHUB_DEVICE_CONN_STRING"" -r $sampleRunningTimeInSeconds"
         RunApp iot-hub\Samples\device\FileUploadSample "IoTHub\Device\FileUploadSample" "-p ""$env:IOTHUB_DEVICE_CONN_STRING"""
         RunApp iot-hub\Samples\device\MessageReceiveSample "IoTHub\Device\MessageReceiveSample" "-p ""$env:IOTHUB_DEVICE_CONN_STRING"" -r $sampleRunningTimeInSeconds"
@@ -119,30 +118,60 @@ try {
         $pnpDeviceSecurityType = "connectionString"
         RunApp iot-hub\Samples\device\PnpDeviceSamples\TemperatureController "IoTHub\Device\PnpDeviceSamples\TemperatureController" "--DeviceSecurityType $pnpDeviceSecurityType -p ""$env:PNP_TC_DEVICE_CONN_STRING"" -r $sampleRunningTimeInSeconds"
         RunApp iot-hub\Samples\device\PnpDeviceSamples\Thermostat "IoTHub\Device\PnpDeviceSamples\Thermostat" "--DeviceSecurityType $pnpDeviceSecurityType -p ""$env:PNP_THERMOSTAT_DEVICE_CONN_STRING"" -r $sampleRunningTimeInSeconds"
-        # DeviceStreaming sample is not added since it is not available in all regions.
 
+        # Run the iot-hub\module sample
         RunApp iot-hub\Samples\module\ModuleSample "IoTHub\Module\ModuleSample" "-p ""$env:IOTHUB_MODULE_CONN_STRING"" -r $sampleRunningTimeInSeconds"
 
+        # Run the iot-hub\service samples
+        $deviceId = ($Env:IOTHUB_DEVICE_CONN_STRING.Split(';') | Where-Object {$_ -like "DeviceId=*"}).Split("=")[1]
+        $iothubHost = ($Env:IOTHUB_CONNECTION_STRING.Split(';') | Where-Object {$_ -like "HostName=*"}).Split("=")[1]
+
         RunApp iot-hub\Samples\service\AutomaticDeviceManagementSample "IoTHub\Service\AutomaticDeviceManagementSample"
+        RunApp iot-hub\Samples\service\AzureSasCredentialAuthenticationSample "IoTHub\Service\AzureSasCredentialAuthenticationSample" "-r $iothubHost -d $deviceId -s ""$env:IOT_HUB_SAS_KEY"" -n ""$env:IOT_HUB_SAS_KEY_NAME"""
         RunApp iot-hub\Samples\service\EdgeDeploymentSample "IoTHub\Service\EdgeDeploymentSample"
         RunApp iot-hub\Samples\service\JobsSample "IoTHub\Service\JobsSample"
         RunApp iot-hub\Samples\service\RegistryManagerSample "IoTHub\Service\RegistryManagerSample" "-c ""$env:IOTHUB_CONNECTION_STRING"" -p ""$env:IOTHUB_PFX_X509_THUMBPRINT"""
 
-        $deviceId = ($Env:IOTHUB_DEVICE_CONN_STRING.Split(';') | where {$_ -like "DeviceId*"}).Split("=")[1]
         Write-Warning "Using device $deviceId for the ServiceClientSample."
         RunApp iot-hub\Samples\service\ServiceClientSample "IoTHub\Service\ServiceClientSample" "-c ""$env:IOTHUB_CONNECTION_STRING"" -d $deviceId -r $sampleRunningTimeInSeconds"
-        # DigitalTwinClientSamples and PnpServiceSamples are not added here since they require the device counterparts to be running as well.
 
-        # TODO #11: Modify Provisioning\device samples to run unattended.
+        # Run provisioning\device samples
+        #RunApp provisioning\Samples\device\ComputeDerivedSymmetricKeySample
+        #RunApp provisioning\Samples\device\SymmetricKeySample
+        #RunApp provisioning\Samples\device\TpmSample
+        #RunApp provisioning\Samples\device\X509Sample
 
+        # Run provisioning\service samples
         RunApp provisioning\Samples\service\BulkOperationSample "Provisioning\Service\BulkOperationSample"
-        # TODO #11 :RunApp provisioning\Samples\service\EnrollmentGroupSample "Provisioning\Service\EnrollmentGroupSample"
         RunApp provisioning\Samples\service\EnrollmentSample "Provisioning\Service\EnrollmentSample"
 
-        # IoT Hub devices and DPS enrollments cleanup
+        # Run the cleanup again so that identities and enrollments created for the samples are cleaned up.
         RunApp provisioning\Samples\service\CleanupEnrollmentsSample "Provisioning\Service\CleanupEnrollmentsSample"
         RunApp iot-hub\Samples\service\CleanupDevicesSample "IoTHub\Service\CleanupDevicesSample" "-c ""$env:IOTHUB_CONNECTION_STRING"" -a ""$env:STORAGE_ACCOUNT_CONNECTION_STRING"" --PathToDevicePrefixForDeletion ""$env:PATH_TO_DEVICE_PREFIX_FOR_DELETION_FILE"""
         RunApp iot-hub\Samples\service\CleanupDevicesSample "IoTHub\Service\CleanupDevicesSample" "-c ""$env:FAR_AWAY_IOTHUB_CONNECTION_STRING"" -a ""$env:STORAGE_ACCOUNT_CONNECTION_STRING"" --PathToDevicePrefixForDeletion ""$env:PATH_TO_DEVICE_PREFIX_FOR_DELETION_FILE"""
+
+        # These samples are currently not added to the pipeilne run. The open items against them need to be addressed before they can be added to the pipeline run.
+
+        # Ignore: DeviceStreaming creates a bi-directional tunnel to enable client-server communication. These device and service samples need to be merged so that they have both the device and service components.
+        # DeviceStreaming is available in Central US EUAP, so we can run this against our preview pipelines.
+        # For more details on regional availability of device streams, see https://docs.microsoft.com/en-us/azure/iot-hub/iot-hub-device-streams-overview#regional-availability.
+        # RunApp iot-hub\Samples\device\DeviceStreamingSample "IoTHub\Device\DeviceStreamingSample" "-p ""$env:IOTHUB_DEVICE_CONN_STRING"""
+        # $deviceId = ($Env:IOTHUB_DEVICE_CONN_STRING.Split(';') | where {$_ -like "DeviceId*"}).Split("=")[1]
+        # Write-Warning "Using device $deviceId for the DeviceStreamingSample."
+        # RunApp iot-hub\Samples\service\DeviceStreamingSample "IoTHub\Service\DeviceStreamingSample" "-c ""$env:IOTHUB_CONNECTION_STRING"" -d $deviceId"
+
+        # Ignore: X509DeviceCertWithChainSample has some pre-requisites associated. Need to make sure that they can run unattended.
+
+        # Ignore: ImportExportDevicesSample needs to be refactored to accept command-line parameters.
+        
+        # Ignore: ImportExportDevicesWithManagedIdentitySample has some pre-requisites associated. Need to ensure the managed identity can access the storage accounts.
+
+        # Ignore: RoleBasedAuthenticationSample requires an AAD app to be set up.
+
+        # Ignore: EnrollmentGroupSample needs to be refactored to accept command-line parameters.
+        
+        # Ignore: DigitalTwinClientSamples requires device-side counterpart to run.
+        # Ignore: PnpServiceSamples requires device-side counterpart to run.
     }
 
     $buildFailed = $false
@@ -152,7 +181,7 @@ catch [Exception]{
     $errorMessage = $Error[0]
 }
 finally {
-    cd $rootDir
+    Set-Location $rootDir
     $endTime = Get-Date
 }
 
