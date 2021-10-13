7816d5f2141b8d119c9502eb4d8a6050042ec707
Azad Abbasi <azabbasi@microsoft.com>
2021-10-13 08:33:19 -0700

    Some updates

--- iot-hub/Samples/service/FileUploadNotificationReceiverSample/FileUploadNotificationReceiverSample.cs
+++ iot-hub/Samples/service/FileUploadNotificationReceiverSample/FileUploadNotificationReceiverSample.cs
@@ -14,8 +14,9 @@ namespace Microsoft.Azure.Devices.Samples
         private readonly ILogger _logger;
         private readonly TransportType _transportType;
         private static ServiceClient _serviceClient;
+        private static readonly TimeSpan s_notificationReceiverTimeout = TimeSpan.FromSeconds(5);
 
-        public FileUploadNotificationReceiverSample(String iotHubConnectionString, TransportType transportType, ILogger logger)
+        public FileUploadNotificationReceiverSample(string iotHubConnectionString, TransportType transportType, ILogger logger)
         {
             _iotHubConnectionString = iotHubConnectionString ?? throw new ArgumentNullException(nameof(iotHubConnectionString));
             _transportType = transportType;
@@ -45,11 +46,44 @@ namespace Microsoft.Azure.Devices.Samples
 
         private async Task ReceiveFileUploadNotifications(CancellationToken cancellationToken)
         {
-            _logger.LogInformation($"\nListening for file upload notifications from the service.");
-            while(!cancellationToken.IsCancellationRequested)
+            _logger.LogInformation($"Listening for file upload notifications from the service.");
+            FileNotificationReceiver<FileNotification> notificationReceiver = _serviceClient.GetFileNotificationReceiver();
+            int totalNotificationsReceived = 0;
+            while (!cancellationToken.IsCancellationRequested)
             {
+                try
+                {
+                    FileNotification fileUploadNotification = await notificationReceiver.ReceiveAsync(s_notificationReceiverTimeout);
 
+                    if (fileUploadNotification == null)
+                    {
+                        _logger.LogInformation($"Did not receive any notification after {s_notificationReceiverTimeout.TotalSeconds} seconds.");
+                        continue;
+                    }
+
+                    totalNotificationsReceived++;
+
+                    _logger.LogInformation($"Received file upload notification.");
+                    _logger.LogInformation($"\tDeviceId: {fileUploadNotification.DeviceId ?? "N/A"}.");
+                    _logger.LogInformation($"\tFileName: {fileUploadNotification.BlobName ?? "N/A"}.");
+                    _logger.LogInformation($"\tEnqueueTimeUTC: {fileUploadNotification.EnqueuedTimeUtc}.");
+                    _logger.LogInformation($"\tBlobSizeInBytes: {fileUploadNotification.BlobSizeInBytes}.");
+
+                    _logger.LogInformation($"Marking notification for {fileUploadNotification.DeviceId} as complete.");
+
+                    await notificationReceiver.CompleteAsync(fileUploadNotification);
+
+                    _logger.LogInformation($"Successfully marked the notification for device {fileUploadNotification.DeviceId} as completed.");
+                }
+                catch (Exception e)
+                {
+                    _logger.LogInformation($"Caught an exception: {e.Message} - {e}");
+                }
             }
+
+            _logger.LogInformation($"Total Notifications Received: {totalNotificationsReceived}.");
+            _logger.LogInformation($"Closing the service client.");
+            await _serviceClient.CloseAsync();
         }
 
         private async Task InitializeServiceClientAsync()
