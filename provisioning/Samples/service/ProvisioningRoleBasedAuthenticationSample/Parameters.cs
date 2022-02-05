79eacc39a53fddb404ec2ebef59a34eb4e1d17a0
dylanbulfinMS <95251881+dylanbulfinMS@users.noreply.github.com>
2022-02-04 20:15:13 -0500

    Add TokenCredential sample

---
+++ provisioning/Samples/service/ProvisioningRoleBasedAuthenticationSample/Parameters.cs
@@ -0,0 +1,45 @@
+﻿using CommandLine;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace ProvisioningRoleBasedAuthenticationSample
+{
+    /// <summary>
+    /// Parameters for the sample
+    /// </summary>
+    internal class Parameters
+    {
+        [Option(
+            'h',
+            "HostName",
+            Required = true,
+            HelpText = "The DPS host name. Ex: my-dps.azure-devices-provisioning.net")]
+        public string HostName { get; set; }
+
+        [Option(
+            't',
+            "TenantId",
+            Required = true,
+            HelpText = "The Azure Active Directory tenant (directory) Id." +
+            " This sample uses ClientSecretCredential. For other ways to use role based authentication, see https://docs.microsoft.com/dotnet/api/azure.identity?view=azure-dotnet.")]
+        public string TenantId { get; set; }
+
+        [Option(
+            'c',
+            "ClientId",
+            Required = true,
+            HelpText = "The client Id of the Azure Active Directory application." +
+            " This sample uses ClientSecretCredential. For other ways to use role based authentication, see https://docs.microsoft.com/dotnet/api/azure.identity?view=azure-dotnet.")]
+        public string ClientId { get; set; }
+
+        [Option(
+            's',
+            "ClientSecret",
+            Required = true,
+            HelpText = "A client secret that was generated for the application Registration used to authenticate the client." +
+            " This sample uses ClientSecretCredential. For other ways to use role based authentication, see https://docs.microsoft.com/dotnet/api/azure.identity?view=azure-dotnet.")]
+        public string ClientSecret { get; set; }
+    }
+}
