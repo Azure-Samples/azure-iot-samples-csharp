db9766bfaefe7887093f3f9f7bd68f55e335a6f9
Nicholas Patilsen <npatilsen@microsoft.com>
2022-08-26 13:19:09 -0700

    nit fix

--- iot-hub/Samples/service/JobsSample/Parameters.cs
+++ iot-hub/Samples/service/JobsSample/Parameters.cs
@@ -6,7 +6,7 @@ namespace Microsoft.Azure.Devices.Samples.JobsSample
     /// <summary>
     /// Parameters for the application.
     /// </summary>
-    ///     /// <remarks>
+    /// <remarks>
     /// To get these connection strings, log into https://portal.azure.com, go to Resources, open the IoT hub, open Shared Access Policies, open iothubowner, and copy a connection string.
     /// </remarks>
     internal class Parameters
